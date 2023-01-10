fe47a13dbd088bc848468901d43492a011f42956
allenhalsay <allenhalsay@gmail.com>
2023-01-10 18:39:15 -0300

    Enviando exemplo de variáveis de acordo com seu tipo

--- mod8/repositorio-allen-mod8/src/br/com/allen/Exemplos.java
+++ mod8/repositorio-allen-mod8/src/br/com/allen/Exemplos.java
@@ -2,9 +2,31 @@ package br.com.allen;
 
 /**
  * 
- * @author Allen
+ * @author Allen Halsay
  *
  */
 public class Exemplos {
 	
+	//Mais usados
+	private int codigo=1;
+	private long codigoMaior=1231980;
+	private boolean status=false;
+	private double valorDecimal1=10.9;
+	private String texto="Allen Halsay";
+	
+	//Menos usados
+	private float valorDecimal=10.9f;
+	private short shor;
+	private byte bi;
+	
+	//Métodos
+	public String retornaTexto() {
+		return "texto";
+	}
+	public int retornaInteiro() {
+		return 10;
+	}
+	public long retornaLong(long num) {
+		return num;
+	}
 }
